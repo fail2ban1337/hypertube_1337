89fe054f348f13e141a118c63d4eabb1743a3bc4
fdrissi <drissi.toubbali.fadel@gmail.com>
2020-01-22 23:41:57 +0100

    Add Library page. - List Movies. - Filter and sort movies. - Infinite scroll

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,4 +1,10 @@
-export const SET_ALERT = 'SET_ALERT';
-export const REMOVE_ALERT = 'REMOVE_ALERT';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const SET_LOADING = "SET_LOADING";
+export const SET_MOVIES = "SET_MOVIES";
+export const UNSET_MOVIES = "UNSET_MOVIES";
+export const SET_SORT = "SET_SORT";
+export const SET_FILTERS = "SET_FILTERS";
+export const SET_HASMORE = "SET_HASMORE";
