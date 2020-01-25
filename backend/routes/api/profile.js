d0b00a1d12ef1c5aadbee8c111751cc32cd5451c
fdrissi <drissi.toubbali.fadel@gmail.com>
2020-01-25 20:08:08 +0100

    Upload profile image

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,5 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const config = require("config");
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    const dir = config.get("profileImages");
+    fs.exists(dir, exists => {
+      if (!exists) {
+        return fs.mkdir(dir, error => cb(error, dir));
+      }
+      return cb(null, dir);
+    });
+  },
+  filename: function(req, file, callback) {
+    return callback(
+      null,
+      "IMAGE-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+const upload = multer({ storage: storage });
+
 const midlleware = require("../../middleware/midlleware");
 const profileModel = require("../../models/Profile");
 const userModel = require("../../models/User");
@@ -20,6 +44,17 @@ router.get("/me", [midlleware.auth], async (req, res) => {
       });
     }
     res.json(profile);
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   POST api/profile/image
+// @desc    Upload profile image
+// @access  Private
+router.post("/image", upload.single("profileImage"), async (req, res) => {
+  try {
+    res.json(req.file.filename);
   } catch (error) {
     res.status(500).send("Server Error");
   }
--- client/src/Components/profile/ProfileHeader.js
+++ client/src/Components/profile/ProfileHeader.js
@@ -1,7 +1,20 @@
-import React from "react";
-import { Grid, Avatar, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import UploadProfile from "../editProfile/UploadProfile";
+
+const useStyles = makeStyles({
+  title: {
+    color: "#939598",
+    fontWeight: "900",
+    letterSpacing: "20px"
+  }
+});
 
 export const ProfileHeader = () => {
+  const classes = useStyles();
+
   return (
     <Grid
       container
@@ -9,15 +22,20 @@ export const ProfileHeader = () => {
       alignItems="center"
       style={{ padding: 20 }}
     >
-      <Grid item xs={12}>
+      <Grid item xs={12} style={{ position: "relative" }}>
+        <UploadProfile />
+        {/* 
+          if not user own profile
         <Avatar
-          src="https://writestylesonline.com/wp-content/uploads/2016/08/Follow-These-Steps-for-a-Flawless-Professional-Profile-Picture-1024x1024.jpg"
-          alt="profile-image"
-          style={{ width: "150px", height: "150px" }}
-        />
+            src="https://writestylesonline.com/wp-content/uploads/2016/08/Follow-These-Steps-for-a-Flawless-Professional-Profile-Picture-1024x1024.jpg"
+            alt="avatar"
+            style={{ width: "150px", height: "150px" }}
+          /> */}
       </Grid>
       <Grid item xs={12}>
-        <Typography variant="h5">Jhon Doe</Typography>
+        <Typography variant="h5" className={classes.title}>
+          Jhon Doe
+        </Typography>
       </Grid>
     </Grid>
   );
