e081f4190f766434cfa9b7e9723cad7b72d187e8
yboumale <starpark@user.local>
2020-02-13 23:34:03 +0100

    add multilanguage Filler controller (en,fr)

--- client/src/i18n/locales/en.js
+++ client/src/i18n/locales/en.js
@@ -7,6 +7,26 @@ const en = {
         dont_have_an_account:'Don\'t have an account? Sign Up',
         or:'OR'
     },
+    controller:{
+        sort:'Sort',
+        title:'Title',
+        rating:'Rating',
+        year:'Year',
+        filter:'Filter',
+        genre:'Gender',
+        genrelist:{
+            action:'Action',
+            adventure:'Adventure',
+            comedy:'Comedy',
+            drama:'Drama',
+            fantasy:'Fantasy',
+            historical:'Historical',
+            romance:'Romance',
+            science_fiction:'Science fiction',
+            western:'Western',
+            all:'All',
+        }
+    },
     footer:{
         made_with:'Made with',
         in_1337:'in 1337'
--- client/src/i18n/locales/fr.js
+++ client/src/i18n/locales/fr.js
@@ -7,6 +7,26 @@ const fr = {
         dont_have_an_account:'Vous n\'avez pas de compte? S\'inscrire',
         or:'OU'
     },
+    controller:{
+        sort:'Trier',
+        title:'Titre',
+        rating:'Avis',
+        year:'Année',
+        filter:'Filtre',
+        genre:'Genre',
+        genrelist:{
+            action:'Action',
+            adventure:'Aventure',
+            comedy:'Comédie',
+            drama:'Drame',
+            fantasy:'Fantaisie',
+            historical:'Historique',
+            romance:'Romance',
+            science_fiction:'Science fiction',
+            western:'Occidental',
+            all:'Tout',
+        }
+    },
     footer:{
         made_with:'Fabriqué avec',
         in_1337:' 1337'
