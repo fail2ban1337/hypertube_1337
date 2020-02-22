d1bd6471ee6e396735ae9a507c1e0dc24ae08485
fdrissi <drissi.toubbali.fadel@gmail.com>
2020-02-22 15:08:42 +0100

    Enhancement

--- client/src/Components/profile/Profile.js
+++ client/src/Components/profile/Profile.js
@@ -9,19 +9,19 @@ import { getProfile } from "../../actions/userAction";
 import { useSelector, useDispatch } from "react-redux";
 
 export const Profile = ({ match }) => {
-  const { profile } = useSelector(state => state);
+  const { profile, user } = useSelector(state => state);
   // profile id from link
   const dispatch = useDispatch();
 
-  let id = match.params.id;
+  let id = match.params.id || user.info._id;
   /*
    * pass profile image to ProfileHeader Component
    * call watched movies route, pass movies to ProfileListWatches
    */
 
   useEffect(() => {
-    dispatch(getProfile(id));
-  }, [id]);
+    if (user.info._id) dispatch(getProfile(id));
+  }, [id, user.info._id]);
 
   if (profile.loading) return <Loading text="Loading profile" />;
   if (!profile.profile) return <Loading text="profile not valide" />;
